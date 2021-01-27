0de3677b787eb580bf826e2b7e4aa2b2af61750b
tlsdmswls <jklkm90@gmail.com>
2021-01-28 00:28:46 +0900

    boolean

---
+++ conditional_statement/boolean.py
@@ -0,0 +1,13 @@
+# 범위 구하기
+x = 5
+print(1 < x < 10)
+print(30 < x < 50)
+
+# not: 반대로 전환
+print(not True)
+
+# and
+print("True and True: ", True and True)
+
+# or
+print("False or True: ", False and True)

c43cf1ca7fdbaacf1b9a4460b59c1cc2554a136d
tlsdmswls <jklkm90@gmail.com>
2021-01-27 13:19:58 +0900

    assignment operators

---
+++ basic/assignment.py
@@ -0,0 +1,18 @@
+pi =  3.14159565
+print("pi: ", pi)
+
+# 숫자 복합 대입 연산자
+num = 10
+num += 2
+num -= 2
+num *= 2
+num /= 2
+# num %= 2
+num **= 2
+print("num: ", num)
+
+# 문자열 복합 대입 연산자
+string = "안녕하세요"
+string += "~~"
+string += "!"      # *= 연산자도 사용 가능
+print("string: ", string)

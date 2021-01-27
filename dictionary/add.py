2074e12cedc2d3333adb1d5bb1735bc15411c8f6
tlsdmswls <jklkm90@gmail.com>
2021-01-28 02:13:59 +0900

    add dictionary

---
+++ dictionary/add.py
@@ -0,0 +1,9 @@
+dictionary = {}
+
+print("요소 추가 전: ", dictionary)
+
+dictionary["name"] = "new name"
+dictionary["head"] = "new head"
+dictionary["body"] = "new body"
+
+print("요소 추가 후: ", dictionary)

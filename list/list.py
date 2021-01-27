50d399fff29fbec55fbdd040eaec0b603334ed4f
tlsdmswls <jklkm90@gmail.com>
2021-01-28 01:33:39 +0900

    list

---
+++ list/list.py
@@ -0,0 +1,14 @@
+array = [1, 23, 5, "abc", True, False]
+
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[1:3])
+
+array[0] = "python"
+
+print(array)
+
+print(array[-1])
+print(array[-2])
+print(array[-3])
